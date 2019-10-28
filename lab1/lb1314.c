72550519158f4605de89775fe7869c1db3431848
interessant0 <1428519881@qq.com>
2019-10-27 20:30:30 -0700

    Enter

--- lab1/lb1314.c
+++ lab1/lb1314.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 int main(){
     print("Celsius to Fahrenheit thermometer");
-    int fahr,celsius;
+    float  fahr,celsius;
     int lower,upper,step;
 
     lower=0;
     upper=100;
-    step=20;
+    step=5;
 
     celsius=lower;
+    printf("celsius\tfahr\n");
     while (celsius<=upper){
-        fahr=(celsius*9/15+32);
-        printf("%3.0f %6.1f\n",fahr,celsius);
+        fahr=(celsius*9.0)/5.0+32.0;
+        printf("%3.0f%6.1f\n",fahr,celsius);
         celsius=celsius+step;
     }
 }
